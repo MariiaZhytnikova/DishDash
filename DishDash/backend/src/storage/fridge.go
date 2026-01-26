7dabf4d44e0942ab76c0c8247cc965a988624797
Mariia Zhytnikova <allusio@gmail.com>
2026-01-26 11:54:02 +0200

    fix file read

--- DishDash/backend/src/storage/fridge.go
+++ DishDash/backend/src/storage/fridge.go
@@ -1,38 +1,29 @@
 package storage
 
 import (
-	"encoding/json"
-	"os"
-	"path/filepath"
-
 	"DishDash/src/models"
 )
 
-func fridgePath() string {
-	return filepath.Join("data", "fridge.json")
-}
-
 func LoadFridge() ([]models.Ingredient, error) {
-	path := fridgePath()
-	data, err := os.ReadFile(path)
+	var list []models.Ingredient
+
+	path, err := DataFile("fridge.json")
 	if err != nil {
-		if os.IsNotExist(err) {
-			return []models.Ingredient{}, nil
-		}
 		return nil, err
 	}
 
-	var list []models.Ingredient
-	if err := json.Unmarshal(data, &list); err != nil {
+	if err := loadJSON(path, &list); err != nil {
 		return nil, err
 	}
+
 	return list, nil
 }
 
 func SaveFridge(list []models.Ingredient) error {
-	data, err := json.MarshalIndent(list, "", "  ")
+	path, err := DataFile("fridge.json")
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(fridgePath(), data, 0644)
+	return saveJSON(path, list)
 }
+
